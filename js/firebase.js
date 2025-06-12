b5836f1591c3c6d30d83fc7f743708071019439d
sh17coder <shauryaji445@gmail.com>
2025-06-12 18:41:22 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const realtimeDb = firebase.database();
+
+// Export Firebase services
+export { db, auth, realtimeDb };
